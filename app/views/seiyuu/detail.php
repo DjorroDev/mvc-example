143379b51a90b329d02b08b09b0bd69293661153
DjorroDev <aufarmawarid@gmail.com>
2022-04-16 22:03:24 +0700

    fixed about desc for new line

--- app/views/seiyuu/detail.php
+++ app/views/seiyuu/detail.php
@@ -5,7 +5,7 @@
         <h5 class="card-title"><?= $data['seiyuu']['name']; ?></h5>
         <h6 class="card-subtitle mb-2 text-muted">Birth: <?= $data['seiyuu']['birth']; ?></h6>
         <h6 class="card-subtitle mb-2"><?= $data['seiyuu']['gender']; ?></h6>
-        <p class="card-text"><?= $data['seiyuu']['about']; ?></p>
+        <p class="card-text"><?= nl2br($data['seiyuu']['about']); ?></p>
         <a href="<?= BASEURL; ?>/seiyuu" class="card-link">back</a>
     </div>
 </div>
