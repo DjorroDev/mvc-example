a0d9ff0185442830c83b9770e9781e9cf441c3a9
DjorroDev <aufarmawarid@gmail.com>
2022-04-17 08:31:28 +0700

    JQuery Added

---
+++ public/js/script.js
@@ -0,0 +1,44 @@
+$(function() {
+
+    // Add button Form
+    $('#buttonAdd').on('click', () => {
+        
+        $('#name').val('');
+        $('#birth').val('');
+        $('#gender').val('Male');
+        $('#about').val('');
+        $('#id').val('');
+
+        $('#modalTitle').html('Add New Seiyuu');
+        $('.modal-footer button[type=submit]').html('Add Data');
+        $('modal-body form').attr('action', 'http://localhost/mvc/public/seiyuu/add');
+
+
+
+    })
+    
+    // Edit button Form
+     $('.showModalEdit').on('click', function() {
+
+        $('#modalTitle').html('Edit Data Seiyuu');
+        $('.modal-footer button[type=submit]').html('Save Changes');
+        $('.modal-body form').attr('action', 'http://localhost/mvc/public/seiyuu/change');
+
+        const id = $(this).data("id");
+
+        $.ajax({
+            url: 'http://localhost/mvc/public/seiyuu/getchange',
+            data: {id : id},
+            method: 'post',
+            dataType: 'json',
+            success: (data) => {
+                $('#name').val(data.name);
+                $('#birth').val(data.birth);
+                $('#gender').val(data.gender);
+                $('#about').val(data.about);
+                $('#id').val(data.id);
+            }
+        });
+
+     });
+});
