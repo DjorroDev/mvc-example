7e229fec1605ce3fb1326846907e1ea15ccb24f6
DjorroDev <aufarmawarid@gmail.com>
2022-04-19 02:47:41 +0700

    added image to detail

--- app/views/seiyuu/detail.php
+++ app/views/seiyuu/detail.php
@@ -1,11 +1,6 @@
-<h1>detail</h1>
-
-<div class="card" style="width: 18rem;">
-    <div class="card-body">
-        <h5 class="card-title"><?= $data['seiyuu']['name']; ?></h5>
-        <h6 class="card-subtitle mb-2 text-muted">Birth: <?= $data['seiyuu']['birth']; ?></h6>
-        <h6 class="card-subtitle mb-2"><?= $data['seiyuu']['gender']; ?></h6>
-        <p class="card-text"><?= nl2br($data['seiyuu']['about']); ?></p>
-        <a href="<?= BASEURL; ?>/seiyuu" class="card-link">back</a>
-    </div>
-</div>
+<h1><?= $data['seiyuu']['name']; ?></h1>
+<img src="<?= BASEURL; ?>/img/people/<?= $data['seiyuu']['image']; ?>" class="mb-3" alt="<?= $data['seiyuu']['name']; ?>">
+<h6 class="mb-2 text-muted">Birth: <?= $data['seiyuu']['birth']; ?></h6>
+<h6 class="mb-2"><?= $data['seiyuu']['gender']; ?></h6>
+<p class="card-text"><?= nl2br($data['seiyuu']['about']); ?></p>
+<a href="<?= BASEURL; ?>/seiyuu" class=" btn btn-primary mb-5">back</a>
